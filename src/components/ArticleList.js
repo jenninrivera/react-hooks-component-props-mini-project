7c811abe0764c459569030a353326c5cc67d4265
Jenni Rivera <jenninrivera@gmail.com>
2023-08-22 12:13:31 -0400

    complete lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import Article from "./Article"
+
+
+function ArticleList({posts}) {
+    const articleComponents = posts.map(post => {
+        return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes}/>
+    })
+    return (
+        <main>
+            {articleComponents}
+        </main>
+)}
+
+
+
+export default ArticleList;
+
+
+
