e632eda8597ddea9f1745eb0f9860a477c8c8766
Jenni Rivera <jenninrivera@gmail.com>
2023-08-22 13:04:35 -0400

    fixed bugs

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,28 +1,28 @@
-function Article({title, date = "January 1, 1970", preview, minutes}) {
-    function displayEmojis() {
-        let emojis =''
-        
-        if(minutes < 30) {
+function Article({title, date = "January 1, 1970", preview, minutes}){
+
+    function displayEmojis(){
+        let emojis = ''
+
+        if(minutes < 30){
             for(let counter = 0; counter < minutes; counter+=5){
-            emojis +='☕️'
+                emojis += '☕️'
             }
-        } 
-        else {
-            for(let counter = 0; counter < minutes; counter+=10) {
-                emojis +='🍱'
+        }
+        else{
+            for(let counter = 0; counter < minutes; counter+=10){
+                emojis += '🍱'
+            }
         }
         return emojis
     }
-}   
-    return(
+
+    return (
         <article>
-        <h3>{title}</h3>
-        <small>{date} • {displayEmojis()} {minutes} min read</small>
-        <p>{preview}</p>
+            <h3>{title}</h3>
+            <small>{date} • {displayEmojis()} {minutes} min read</small>
+            <p>{preview}</p>
         </article>
     )
-
 }
 
-
-export default Article
+export default Article;
